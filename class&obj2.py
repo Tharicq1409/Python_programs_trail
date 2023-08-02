446263ac1eadb8f5efc18877169fe35696ad9e50
Tharicq1409 <sheiktharicq14@gmail.com>
2023-08-02 21:49:33 +0530

    Day2 file added

---
+++ class&obj2.py
@@ -0,0 +1,20 @@
+'''
+class student:
+    def __ini__(self): #constructor
+        self.name=""    #creating variables inside constructor
+        self.register_no=""
+    def display(self):
+        print("Register Number :",self.register_no)
+        print("Name :",self.name)
+
+s1=student()
+s1.name = "Ram"
+s1.register_no = "001"
+s1.display()
+
+
+s2=student()
+s2.name= "John"
+s2.register_no = "002"
+s2.display()'''
+
